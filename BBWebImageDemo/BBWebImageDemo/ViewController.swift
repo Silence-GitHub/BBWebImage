72b12fb5ab74fdeca964e919aff3278f2174c1d0
Kaibo Lu <kaibo@KaibodeiMac.local>
2018-10-03 15:49:12 -0700

    Add BBWebImageDemo project

---
+++ BBWebImageDemo/BBWebImageDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BBWebImageDemo
+//
+//  Created by Kaibo Lu on 2018/10/3.
+//  Copyright © 2018年 Kaibo Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
