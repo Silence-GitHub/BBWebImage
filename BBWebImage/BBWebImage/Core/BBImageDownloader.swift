68db2d7404f88e2d7bd62fc1f1ab5d66ed131a06
Kaibo Lu <kaibo@KaibodeiMac.local>
2018-10-04 12:28:49 -0700

    Add BBImageDownloader functions

--- BBWebImage/BBWebImage/Core/BBImageDownloader.swift
+++ BBWebImage/BBWebImage/Core/BBImageDownloader.swift
@@ -8,10 +8,47 @@
 
 import UIKit
 
+typealias BBImageDownloaderCompletion = (Data?, Error?) -> Void
+
+protocol BBImageDownloadTask {
+    var isCancelled: Bool { get }
+    var completion: BBImageDownloaderCompletion { get }
+}
+
 protocol BBImageDownloader {
+    // Donwload
+    func downloadImage(with url: URL, completion: @escaping BBImageDownloaderCompletion) -> BBImageDownloadTask
     
+    // Cancel
+    func cancel(task: BBImageDownloadTask)
+    func cancel(url: URL)
+    func cancelAll()
+}
+
+struct BBImageDefaultDownloadTask: BBImageDownloadTask {
+    var cancelled: Bool
+    var isCancelled: Bool { return cancelled }
+    var completionHandler: BBImageDownloaderCompletion
+    var completion: BBImageDownloaderCompletion { return completionHandler }
 }
 
 class BBMergeRequestImageDownloader: BBImageDownloader {
-
+    // Donwload
+    func downloadImage(with url: URL, completion: @escaping BBImageDownloaderCompletion) -> BBImageDownloadTask {
+        #warning ("Download image")
+        return BBImageDefaultDownloadTask(cancelled: false, completionHandler: completion)
+    }
+    
+    // Cancel
+    func cancel(task: BBImageDownloadTask) {
+        #warning ("Cancel download task")
+    }
+    
+    func cancel(url: URL) {
+        #warning ("Cancel download url")
+    }
+    
+    func cancelAll() {
+        #warning ("Cancel download all")
+    }
 }
