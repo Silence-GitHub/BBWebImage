0259d48a27169eb984f6a5d2a61f27f2634e85a9
Kaibo Lu <kaibo@KaibodeiMac.local>
2018-10-31 11:33:03 -0700

    Add BBDiskCache

--- BBWebImage/BBWebImage/Core/BBDiskCache.swift
+++ BBWebImage/BBWebImage/Core/BBDiskCache.swift
@@ -9,5 +9,41 @@
 import UIKit
 
 public class BBDiskCache {
-
+    let storage: BBDiskStorage
+    let sizeThreshold: Int
+    let queue: DispatchQueue // Concurrent
+    
+    init?(path: String, sizeThreshold threshold: Int) {
+        if let currentStorage = BBDiskStorage(path: path) {
+            storage = currentStorage
+        } else {
+            return nil
+        }
+        sizeThreshold = threshold
+        queue = DispatchQueue(label: "com.Kaibo.BBWebImage.DiskCache.queue", qos: .utility, attributes: .concurrent)
+        // TODO: Trim by time, size, count
+    }
+    
+    public func data(forKey key: String) -> Data? {
+        return storage.data(forKey: key)
+    }
+    
+    public func data(forKey key: String, completion: @escaping (Data?) -> Void) {
+        queue.async { [weak self] in
+            guard let self = self else { return }
+            completion(self.data(forKey: key))
+        }
+    }
+    
+    public func store(_ data: Data, forKey key: String) {
+        storage.store(data, forKey: key, type: (data.count > sizeThreshold ? .file : .sqlite))
+    }
+    
+    public func store(_ data: Data, forKey key: String, completion: @escaping () -> Void) {
+        queue.async { [weak self] in
+            guard let self = self else { return }
+            self.store(data, forKey: key)
+            completion()
+        }
+    }
 }
