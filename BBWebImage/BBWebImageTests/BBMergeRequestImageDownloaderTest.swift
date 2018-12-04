77b170bc52ae497d72003723f68938b45ce80e92
Kaibo Lu <kaibo@KaibodeiMac.local>
2018-12-04 12:26:55 -0800

    Add downloader test functions

--- BBWebImage/BBWebImageTests/BBMergeRequestImageDownloaderTest.swift
+++ BBWebImage/BBWebImageTests/BBMergeRequestImageDownloaderTest.swift
@@ -59,6 +59,55 @@ class BBMergeRequestImageDownloaderTest: XCTestCase {
         waitForExpectations(timeout: 10, handler: nil)
     }
     
+    func testCancelTaskAndDownload() {
+        let expectation = self.expectation(description: "Wait for downloading image")
+        let url = urls.first!
+        let task = downloader.downloadImage(with: url, options: .none) { (data, error) in
+            XCTFail()
+        }
+        downloader.cancel(task: task)
+        downloader.downloadImage(with: url, options: .none) { (data, error) in
+            XCTAssertNotNil(data)
+            XCTAssertNil(error)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testCancelTaskAndDownloadRepeatedly() {
+        let url = urls.first!
+        for _ in 0..<10 {
+            let task = downloader.downloadImage(with: url, options: .none) { (data, error) in
+                XCTFail()
+            }
+            downloader.cancel(task: task)
+        }
+        let expectation = self.expectation(description: "Wait for downloading image")
+        downloader.downloadImage(with: url, options: .none) { (data, error) in
+            XCTAssertNotNil(data)
+            XCTAssertNil(error)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testCancelTaskAndDownloadRepeatedly2() {
+        let url = urls.first!
+        for _ in 0..<10 {
+            let expectation = self.expectation(description: "Wait for downloading image")
+            let task = downloader.downloadImage(with: url, options: .none) { (data, error) in
+                XCTFail()
+            }
+            downloader.cancel(task: task)
+            downloader.downloadImage(with: url, options: .none) { (data, error) in
+                XCTAssertNotNil(data)
+                XCTAssertNil(error)
+                expectation.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
     func testCancelTasks() {
         let expectation = self.expectation(description: "Wait for downloading image")
         var tasks: [BBImageDownloadTask] = []
@@ -78,16 +127,243 @@ class BBMergeRequestImageDownloaderTest: XCTestCase {
         waitForExpectations(timeout: 10, handler: nil)
     }
     
-    func testCancelAndDownload() {
+    func testCancelTasksAndDownload() {
+        for url in urls {
+            let expectation = self.expectation(description: "Wait for downloading image")
+            let task = downloader.downloadImage(with: url, options: .none) { (data, error) in
+                XCTFail()
+            }
+            downloader.cancel(task: task)
+            downloader.downloadImage(with: url, options: .none) { (data, error) in
+                XCTAssertNotNil(data)
+                XCTAssertNil(error)
+                expectation.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testCancelTasksAndDownloadRepeatedly() {
+        for _ in 0..<10 {
+            for url in urls {
+                let expectation = self.expectation(description: "Wait for downloading image")
+                let task = downloader.downloadImage(with: url, options: .none) { (data, error) in
+                    XCTFail()
+                }
+                downloader.cancel(task: task)
+                downloader.downloadImage(with: url, options: .none) { (data, error) in
+                    XCTAssertNotNil(data)
+                    XCTAssertNil(error)
+                    expectation.fulfill()
+                }
+            }
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testCancelSomeTasks() {
+        var i = 0
+        for url in urls {
+            let shouldCancel = (i % 2 == 0)
+            let expectation: XCTestExpectation? = shouldCancel ? nil : self.expectation(description: "Wait for downloading image")
+            let task = downloader.downloadImage(with: url, options: .none) { (data, error) in
+                if shouldCancel {
+                    XCTFail()
+                } else {
+                    XCTAssertNotNil(data)
+                    XCTAssertNil(error)
+                    expectation!.fulfill()
+                }
+            }
+            if shouldCancel {
+                downloader.cancel(task: task)
+            }
+            i += 1
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testCancelSomeTasksAndDownload() {
+        var i = 0
+        for url in urls {
+            let shouldCancel = (i % 2 == 0)
+            let expectation: XCTestExpectation? = shouldCancel ? nil : self.expectation(description: "Wait for downloading image")
+            let task = downloader.downloadImage(with: url, options: .none) { (data, error) in
+                if shouldCancel {
+                    XCTFail()
+                } else {
+                    XCTAssertNotNil(data)
+                    XCTAssertNil(error)
+                    expectation!.fulfill()
+                }
+            }
+            if shouldCancel {
+                downloader.cancel(task: task)
+                let expectation2 = self.expectation(description: "Wait for downloading image")
+                downloader.downloadImage(with: url, options: .none) { (data, error) in
+                    XCTAssertNotNil(data)
+                    XCTAssertNil(error)
+                    expectation2.fulfill()
+                }
+            }
+            i += 1
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testCancelUrl() {
         let expectation = self.expectation(description: "Wait for downloading image")
         let url = urls.first!
-        let task = downloader.downloadImage(with: url, options: .none) { (data, error) in
+        downloader.downloadImage(with: url, options: .none) { (data, error) in
             XCTFail()
         }
-        downloader.cancel(task: task)
+        downloader.cancel(url: url)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            XCTAssertEqual(self.downloader.currentDownloadCount, 0)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testCancelUrl2() {
+        let expectation = self.expectation(description: "Wait for downloading image")
+        let url = urls.first!
+        for _ in 0..<10 {
+            downloader.downloadImage(with: url, options: .none) { (data, error) in
+                XCTFail()
+            }
+        }
+        downloader.cancel(url: url)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            XCTAssertEqual(self.downloader.currentDownloadCount, 0)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testCancelUrlAndDownload() {
+        let expectation = self.expectation(description: "Wait for downloading image")
+        let url = urls.first!
+        downloader.downloadImage(with: url, options: .none) { (data, error) in
+            XCTFail()
+        }
+        downloader.cancel(url: url)
         downloader.downloadImage(with: url, options: .none) { (data, error) in
             XCTAssertNotNil(data)
             XCTAssertNil(error)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testCancelUrlAndDownloadRepeatedly() {
+        let url = urls.first!
+        for _ in 0..<10 {
+            downloader.downloadImage(with: url, options: .none) { (data, error) in
+                XCTFail()
+            }
+            downloader.cancel(url: url)
+        }
+        let expectation = self.expectation(description: "Wait for downloading image")
+        downloader.downloadImage(with: url, options: .none) { (data, error) in
+            XCTAssertNotNil(data)
+            XCTAssertNil(error)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testCancelUrls() {
+        for url in urls {
+            let expectation = self.expectation(description: "Wait for downloading image")
+            downloader.downloadImage(with: url, options: .none) { (data, error) in
+                XCTFail()
+            }
+            downloader.cancel(url: url)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                XCTAssertEqual(self.downloader.currentDownloadCount, 0)
+                expectation.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testCancelUrlsAndDownload() {
+        for url in urls {
+            let expectation = self.expectation(description: "Wait for downloading image")
+            downloader.downloadImage(with: url, options: .none) { (data, error) in
+                XCTFail()
+            }
+            downloader.cancel(url: url)
+            downloader.downloadImage(with: url, options: .none) { (data, error) in
+                XCTAssertNotNil(data)
+                XCTAssertNil(error)
+                expectation.fulfill()
+            }
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testCancelSomeUrls() {
+        var i = 0
+        for url in urls {
+            let shouldCancel = (i % 2 == 0)
+            let expectation: XCTestExpectation? = shouldCancel ? nil : self.expectation(description: "Wait for downloading image")
+            downloader.downloadImage(with: url, options: .none) { (data, error) in
+                if shouldCancel {
+                    XCTFail()
+                } else {
+                    XCTAssertNotNil(data)
+                    XCTAssertNil(error)
+                    expectation!.fulfill()
+                }
+            }
+            if shouldCancel {
+                downloader.cancel(url: url)
+            }
+            i += 1
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testCancelSomeUrlsAndDownload() {
+        var i = 0
+        for url in urls {
+            let shouldCancel = (i % 2 == 0)
+            let expectation: XCTestExpectation? = shouldCancel ? nil : self.expectation(description: "Wait for downloading image")
+            downloader.downloadImage(with: url, options: .none) { (data, error) in
+                if shouldCancel {
+                    XCTFail()
+                } else {
+                    XCTAssertNotNil(data)
+                    XCTAssertNil(error)
+                    expectation!.fulfill()
+                }
+            }
+            if shouldCancel {
+                downloader.cancel(url: url)
+                let expectation2 = self.expectation(description: "Wait for downloading image")
+                downloader.downloadImage(with: url, options: .none) { (data, error) in
+                    XCTAssertNotNil(data)
+                    XCTAssertNil(error)
+                    expectation2.fulfill()
+                }
+            }
+            i += 1
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+    
+    func testCancelAll() {
+        let expectation = self.expectation(description: "Wait for downloading image")
+        for url in urls {
+            downloader.downloadImage(with: url, options: .none) { (data, error) in
+                XCTFail()
+            }
+        }
+        downloader.cancelAll()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            XCTAssertEqual(self.downloader.currentDownloadCount, 0)
             expectation.fulfill()
         }
         waitForExpectations(timeout: 10, handler: nil)
