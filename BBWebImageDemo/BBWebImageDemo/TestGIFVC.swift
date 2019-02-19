1a19768feb4dcdeff1074de29254233e64a1466e
Kaibo Lu <kaibo@kaibodeimac.attlocal.net>
2019-02-19 10:15:36 -0800

    Update TestGIFVC

--- BBWebImageDemo/BBWebImageDemo/TestGIFVC.swift
+++ BBWebImageDemo/BBWebImageDemo/TestGIFVC.swift
@@ -10,18 +10,97 @@ import UIKit
 import BBWebImage
 
 class TestGIFVC: UIViewController {
+    
+    private var imageView: BBAnimatedImageView!
+    private var scaleLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .lightGray
         
-        let imageView = BBAnimatedImageView(frame: CGRect(x: 10, y: 100, width: view.frame.width - 20, height: view.frame.height - 200))
+        let x: CGFloat = 10
+        let width = view.frame.width - 20
+        
+        let scrollView = UIScrollView(frame: CGRect(x: x, y: 100, width: width, height: 200))
+        scrollView.contentSize = CGSize(width: width + 20, height: 200)
+        view.addSubview(scrollView)
+        
+        imageView = BBAnimatedImageView(frame: scrollView.bounds)
         imageView.backgroundColor = .yellow
         imageView.contentMode = .scaleAspectFit
         let url = Bundle(for: self.classForCoder).url(forResource: "Rotating_earth", withExtension: "gif")!
         let data = try! Data(contentsOf: url)
         imageView.image = BBAnimatedImage(bb_data: data)
-        view.addSubview(imageView)
+        scrollView.addSubview(imageView)
+        
+        var y = scrollView.frame.maxY + 10
+        
+        scaleLabel = UILabel(frame: CGRect(x: x, y: y, width: width, height: 30))
+        scaleLabel.text = "Duration scale: 1.0"
+        scaleLabel.textAlignment = .center
+        view.addSubview(scaleLabel)
+        
+        y = scaleLabel.frame.maxY
+        
+        let scaleSilder = UISlider(frame: CGRect(x: x + 30, y: y, width: width - 60, height: 30))
+        scaleSilder.minimumValue = 0.2
+        scaleSilder.maximumValue = 5
+        scaleSilder.value = 1
+        scaleSilder.addTarget(self, action: #selector(scaleSliderChanged(_:)), for: .valueChanged)
+        view.addSubview(scaleSilder)
+        
+        y = scaleSilder.frame.maxY + 10
+        
+        var buttonIndex = 0
+        let generateButton = { (title: String?, selectedTitle: String?) -> UIButton in
+            let button = UIButton(frame: CGRect(x: x, y: y, width: self.imageView.frame.width, height: 30))
+            button.backgroundColor = (buttonIndex % 2 == 0 ? .blue : .red)
+            button.setTitle(title, for: .normal)
+            button.setTitle(selectedTitle, for: .selected)
+            self.view.addSubview(button)
+            y = button.frame.maxY
+            buttonIndex += 1
+            return button
+        }
+        
+        let stopButton = generateButton("Stop", "Start")
+        stopButton.addTarget(self, action: #selector(stopButtonClicked(_:)), for: .touchUpInside)
+        
+        let runLoopButton = generateButton("Change to default mode", "Change to common mode")
+        runLoopButton.addTarget(self, action: #selector(runLoopButtonClicked(_:)), for: .touchUpInside)
+        
+        let changeImageButton = generateButton("Show static image", "Show GIF")
+        changeImageButton.addTarget(self, action: #selector(changeImageButtonClicked(_:)), for: .touchUpInside)
+    }
+    
+    @objc private func scaleSliderChanged(_ slider: UISlider) {
+        scaleLabel.text = String(format: "Duration scale: %.1f", slider.value)
+        imageView.bb_animationDurationScale = Double(slider.value)
+    }
+    
+    @objc private func stopButtonClicked(_ button: UIButton) {
+        button.isSelected = !button.isSelected
+        if button.isSelected {
+            imageView.stopAnimating()
+        } else {
+            imageView.startAnimating()
+        }
+    }
+    
+    @objc private func runLoopButtonClicked(_ button: UIButton) {
+        button.isSelected = !button.isSelected
+        imageView.bb_runLoopMode = (button.isSelected ? .default : .common)
+    }
+    
+    @objc private func changeImageButtonClicked(_ button: UIButton) {
+        button.isSelected = !button.isSelected
+        if button.isSelected {
+            imageView.image = UIImage(named: "placeholder")
+        } else {
+            let url = Bundle(for: self.classForCoder).url(forResource: "Rotating_earth", withExtension: "gif")!
+            let data = try! Data(contentsOf: url)
+            imageView.image = BBAnimatedImage(bb_data: data)
+        }
     }
 }
