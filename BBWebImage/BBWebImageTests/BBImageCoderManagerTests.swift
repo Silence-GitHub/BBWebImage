30fb09bf78ca4ee30d9084fc4f05a3bb7ac49b6c
Kaibo Lu <kaibo@kaibodeimac.attlocal.net>
2019-02-19 15:28:13 -0800

    BBImageCoderManagerTests add GIF

--- BBWebImage/BBWebImageTests/BBImageCoderManagerTests.swift
+++ BBWebImage/BBWebImageTests/BBImageCoderManagerTests.swift
@@ -21,6 +21,11 @@ class BBImageCoderManagerTests: XCTestCase {
         return try! Data(contentsOf: url)
     }
     
+    var gifData: Data {
+        let url = Bundle(for: classForCoder).url(forResource: "Rotating_earth", withExtension: "gif")!
+        return try! Data(contentsOf: url)
+    }
+    
     override func setUp() {
         coder = BBImageCoderManager()
     }
@@ -31,6 +36,7 @@ class BBImageCoderManagerTests: XCTestCase {
         XCTAssertFalse(coder.canDecode(Data()))
         XCTAssertTrue(coder.canDecode(pngData))
         XCTAssertTrue(coder.canDecode(jpgData))
+        XCTAssertTrue(coder.canDecode(gifData))
     }
     
     func testDecode() {
@@ -43,6 +49,11 @@ class BBImageCoderManagerTests: XCTestCase {
         let jpgImage = coder.decodedImage(with: jpgData)
         XCTAssertNotNil(jpgImage)
         XCTAssertEqual(jpgImage?.bb_imageFormat, .JPEG)
+        
+        let gifImage = coder.decodedImage(with: gifData)
+        XCTAssertNotNil(gifImage)
+        XCTAssertEqual(gifImage?.bb_imageFormat, .GIF)
+        XCTAssertTrue(gifImage is BBAnimatedImage)
     }
     
     func testDecompress() {
@@ -59,19 +70,28 @@ class BBImageCoderManagerTests: XCTestCase {
         XCTAssertNotNil(jpgDecompressedImage)
         XCTAssertNotEqual(jpgImage, jpgDecompressedImage)
         XCTAssertEqual(jpgDecompressedImage?.bb_imageFormat, .JPEG)
+        
+        let gifData = self.gifData
+        let gifImage = coder.decodedImage(with: gifData)!
+        let gifDecompressedImage = coder.decompressedImage(with: gifImage, data: gifData)
+        XCTAssertNil(gifDecompressedImage)
     }
     
     func testCanEncode() {
         XCTAssertTrue(coder.canEncode(.unknown))
         XCTAssertTrue(coder.canEncode(.PNG))
         XCTAssertTrue(coder.canEncode(.JPEG))
+        XCTAssertTrue(coder.canEncode(.GIF))
     }
     
     func testEncode() {
-        let images: [UIImage] = [coder.decodedImage(with: pngData)!, coder.decodedImage(with: jpgData)!]
+        let images: [UIImage] = [coder.decodedImage(with: pngData)!,
+                                 coder.decodedImage(with: jpgData)!,
+                                 coder.decodedImage(with: gifData)!]
         for image in images {
             XCTAssertNotNil(coder.encodedData(with: image, format: .PNG))
             XCTAssertNotNil(coder.encodedData(with: image, format: .JPEG))
+            XCTAssertNotNil(coder.encodedData(with: image, format: .GIF))
             XCTAssertNotNil(coder.encodedData(with: image, format: .unknown))
         }
     }
@@ -79,18 +99,24 @@ class BBImageCoderManagerTests: XCTestCase {
     func testCanIncrementallyDecode() {
         XCTAssertTrue(coder.canIncrementallyDecode(pngData))
         XCTAssertTrue(coder.canIncrementallyDecode(jpgData))
+        XCTAssertFalse(coder.canIncrementallyDecode(gifData))
     }
     
     func testIncrementallyDecodedImage() {
-        let test = { (data: Data, total: Int) -> Void in
+        let test = { (data: Data, total: Int, canDecode: Bool) -> Void in
             for i in 1...total {
                 let finished = (i == total)
                 let end = Int((Double(i) / Double(total)) * Double(data.count))
                 let subdata = data.subdata(in: 0..<end)
-                XCTAssertNotNil(self.coder.incrementallyDecodedImage(with: subdata, finished: finished))
+                if canDecode {
+                    XCTAssertNotNil(self.coder.incrementallyDecodedImage(with: subdata, finished: finished))
+                } else {
+                    XCTAssertNil(self.coder.incrementallyDecodedImage(with: subdata, finished: finished))
+                }
             }
         }
-        test(pngData, 10)
-        test(jpgData, 5) // Need enough data to decode
+        test(pngData, 10, true)
+        test(jpgData, 5, true) // Need enough data to decode
+        test(gifData, 10, false)
     }
 }
