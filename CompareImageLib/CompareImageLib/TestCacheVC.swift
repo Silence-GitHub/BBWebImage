9638741d2eb38c51a0e888ed33e1439ffc813469
Kaibo Lu <lukaibolkb@gmail.com>
2019-03-11 16:35:29 -0700

    Add thumbnail and origin image for disk cache test

--- CompareImageLib/CompareImageLib/TestCacheVC.swift
+++ CompareImageLib/CompareImageLib/TestCacheVC.swift
@@ -25,9 +25,14 @@ private struct TestItem {
     }
 }
 
+extension String: BBWebCacheResource {
+    public var cacheKey: String { return self }
+    public var downloadUrl: URL { return URL(fileURLWithPath: "") }
+}
+
 class TestCacheVC: UIViewController {
     private var list: [(String, NoParamterBlock)]!
-    private let testLoopCount = 1000
+    private let testMemoryLoopCount = 1000
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,7 +46,15 @@ class TestCacheVC: UIViewController {
                 ("Memory origin BBWebImage", testMemoryCacheBB(with: items)),
                 ("Memory origin SDWebImage", testMemoryCacheSD(with: items)),
                 ("Memory origin YYWebImage", testMemoryCacheYY(with: items)),
-                ("Memory origin Kingfisher", testMemoryCacheKi(with: items))]
+                ("Memory origin Kingfisher", testMemoryCacheKi(with: items)),
+                ("Disk thumbnail BBWebImage", testDiskCacheBB(with: thumbnailItems)),
+                ("Disk thumbnail SDWebImage", testDiskCacheSD(with: thumbnailItems)),
+                ("Disk thumbnail YYWebImage", testDiskCacheYY(with: thumbnailItems)),
+                ("Disk thumbnail Kingfisher", testDiskCacheKi(with: thumbnailItems)),
+                ("Disk origin BBWebImage", testDiskCacheBB(with: items)),
+                ("Disk origin SDWebImage", testDiskCacheSD(with: items)),
+                ("Disk origin YYWebImage", testDiskCacheYY(with: items)),
+                ("Disk origin Kingfisher", testDiskCacheKi(with: items))]
         
         let tableView = UITableView(frame: view.bounds)
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.description())
@@ -66,7 +79,7 @@ class TestCacheVC: UIViewController {
             var storeTime: Double = 0
             var getTime: Double = 0
             var removeTime: Double = 0
-            for _ in 0..<self.testLoopCount {
+            for _ in 0..<self.testMemoryLoopCount {
                 var startTime = CACurrentMediaTime()
                 for item in testItems {
                     BBWebImageManager.shared.imageCache.store(item.image, data: nil, forKey: item.key, cacheType: .memory, completion: nil)
@@ -94,12 +107,72 @@ class TestCacheVC: UIViewController {
         }
     }
     
+    private func testDiskCacheBB(with testItems: [TestItem]) -> NoParamterBlock {
+        return {
+            var storeTime: Double = 0
+            var getTime: Double = 0
+            var removeTime: Double = 0
+            func testStore(_ completion: @escaping NoParamterBlock) {
+                var i = testItems.count
+                for item in testItems {
+                    let startTime = CACurrentMediaTime()
+                    BBWebImageManager.shared.imageCache.store(nil, data: item.data, forKey: item.key, cacheType: .disk) {
+                        DispatchQueue.main.async {
+                            i -= 1
+                            if i == 0 {
+                                storeTime += CACurrentMediaTime() - startTime
+                                completion()
+                            }
+                        }
+                    }
+                }
+            }
+            func testGet(_ completion: @escaping NoParamterBlock) {
+                var i = testItems.count
+                for item in testItems {
+                    let startTime = CACurrentMediaTime()
+                    BBWebImageManager.shared.loadImage(with: item.key) { (image, _, _, _) in
+                        assert(image != nil)
+                        i -= 1
+                        if i == 0 {
+                            getTime += CACurrentMediaTime() - startTime
+                            completion()
+                        }
+                    }
+                }
+            }
+            func testRemove(_ completion: @escaping NoParamterBlock) {
+                var i = testItems.count
+                for item in testItems {
+                    let startTime = CACurrentMediaTime()
+                    BBWebImageManager.shared.imageCache.removeImage(forKey: item.key, cacheType: .disk) {
+                        DispatchQueue.main.async {
+                            i -= 1
+                            if i == 0 {
+                                removeTime += CACurrentMediaTime() - startTime
+                                completion()
+                            }
+                        }
+                    }
+                }
+            }
+            testStore {
+                testGet {
+                    testRemove {
+                        print(String(format: "BBWebImage time consume: store %0.4f, get %0.4f, remove %0.4f", storeTime, getTime, removeTime))
+                        BBWebImageManager.shared.imageCache.clear(.memory, completion: nil)
+                    }
+                }
+            }
+        }
+    }
+    
     private func testMemoryCacheSD(with testItems: [TestItem]) -> NoParamterBlock {
         return {
             var storeTime: Double = 0
             var getTime: Double = 0
             var removeTime: Double = 0
-            for _ in 0..<self.testLoopCount {
+            for _ in 0..<self.testMemoryLoopCount {
                 var startTime = CACurrentMediaTime()
                 for item in testItems {
                     SDWebImageManager.shared().imageCache?.store(item.image, imageData: nil, forKey: item.key, toDisk: false, completion: nil)
@@ -123,6 +196,62 @@ class TestCacheVC: UIViewController {
         }
     }
     
+    private func testDiskCacheSD(with testItems: [TestItem]) -> NoParamterBlock {
+        return {
+            SDWebImageManager.shared().imageCache?.config.shouldCacheImagesInMemory = false
+            var storeTime: Double = 0
+            var getTime: Double = 0
+            var removeTime: Double = 0
+            func testStore(_ completion: @escaping NoParamterBlock) {
+                var i = testItems.count
+                for item in testItems {
+                    let startTime = CACurrentMediaTime()
+                    SDWebImageManager.shared().imageCache?.store(item.image, imageData: item.data, forKey: item.key, toDisk: true) {
+                        i -= 1
+                        if i == 0 {
+                            storeTime += CACurrentMediaTime() - startTime
+                            completion()
+                        }
+                    }
+                }
+            }
+            func testGet(_ completion: @escaping NoParamterBlock) {
+                var i = testItems.count
+                for item in testItems {
+                    let startTime = CACurrentMediaTime()
+                    SDWebImageManager.shared().imageCache?.queryCacheOperation(forKey: item.key) { (image, _, _) in
+                        assert(image != nil)
+                        i -= 1
+                        if i == 0 {
+                            getTime += CACurrentMediaTime() - startTime
+                            completion()
+                        }
+                    }
+                }
+            }
+            func testRemove(_ completion: @escaping NoParamterBlock) {
+                var i = testItems.count
+                for item in testItems {
+                    let startTime = CACurrentMediaTime()
+                    SDWebImageManager.shared().imageCache?.removeImage(forKey: item.key, fromDisk: true) {
+                        i -= 1
+                        if i == 0 {
+                            removeTime += CACurrentMediaTime() - startTime
+                            completion()
+                        }
+                    }
+                }
+            }
+            testStore {
+                testGet {
+                    testRemove {
+                        print(String(format: "SDWebImage time consume: store %0.4f, get %0.4f, remove %0.4f", storeTime, getTime, removeTime))
+                    }
+                }
+            }
+        }
+    }
+    
     private func testMemoryCacheYY(with testItems: [TestItem]) -> NoParamterBlock {
         return {
             for item in testItems {
@@ -132,7 +261,7 @@ class TestCacheVC: UIViewController {
             var storeTime: Double = 0
             var getTime: Double = 0
             var removeTime: Double = 0
-            for _ in 0..<self.testLoopCount {
+            for _ in 0..<self.testMemoryLoopCount {
                 var startTime = CACurrentMediaTime()
                 for item in testItems {
                     YYWebImageManager.shared().cache?.setImage(item.image, imageData: nil, forKey: item.key, with: .memory)
@@ -156,12 +285,76 @@ class TestCacheVC: UIViewController {
         }
     }
     
+    private func testDiskCacheYY(with testItems: [TestItem]) -> NoParamterBlock {
+        return {
+            for item in testItems {
+                item.image.yy_isDecodedForDisplay = true
+            }
+            var storeTime: Double = 0
+            var getTime: Double = 0
+            var removeTime: Double = 0
+            func testStore(_ completion: @escaping NoParamterBlock) {
+                var i = testItems.count
+                for item in testItems {
+                    let startTime = CACurrentMediaTime()
+                    YYWebImageManager.shared().cache?.diskCache.setObject(item.data as NSCoding, forKey: item.key) {
+                        DispatchQueue.main.async {
+                            i -= 1
+                            if i == 0 {
+                                storeTime += CACurrentMediaTime() - startTime
+                                completion()
+                            }
+                        }
+                    }
+                }
+            }
+            func testGet(_ completion: @escaping NoParamterBlock) {
+                var i = testItems.count
+                for item in testItems {
+                    let startTime = CACurrentMediaTime()
+                    YYWebImageManager.shared().cache?.getImageForKey(item.key, with: .disk) { (image, _) in
+                        // In main thread
+                        assert(image != nil)
+                        i -= 1
+                        if i == 0 {
+                            getTime += CACurrentMediaTime() - startTime
+                            completion()
+                        }
+                    }
+                }
+            }
+            func testRemove(_ completion: @escaping NoParamterBlock) {
+                var i = testItems.count
+                for item in testItems {
+                    let startTime = CACurrentMediaTime()
+                    YYWebImageManager.shared().cache?.diskCache.removeObject(forKey: item.key) { (_) in
+                        DispatchQueue.main.async {
+                            i -= 1
+                            if i == 0 {
+                                removeTime += CACurrentMediaTime() - startTime
+                                completion()
+                            }
+                        }
+                    }
+                }
+            }
+            testStore {
+                testGet {
+                    testRemove {
+                        print(String(format: "YYWebImage time consume: store %0.4f, get %0.4f, remove %0.4f", storeTime, getTime, removeTime))
+                        YYWebImageManager.shared().cache?.memoryCache.removeAllObjects()
+                    }
+                }
+            }
+        }
+    }
+    
     private func testMemoryCacheKi(with testItems: [TestItem]) -> NoParamterBlock {
         return {
             var storeTime: Double = 0
             var getTime: Double = 0
             var removeTime: Double = 0
-            for _ in 0..<self.testLoopCount {
+            for _ in 0..<self.testMemoryLoopCount {
                 var startTime = CACurrentMediaTime()
                 for item in testItems {
                     KingfisherManager.shared.cache.store(item.image, original: nil, forKey: item.key, toDisk: false, completionHandler: nil)
@@ -182,6 +375,63 @@ class TestCacheVC: UIViewController {
                 removeTime += CACurrentMediaTime() - startTime
             }
             print(String(format: "Kingfisher time consume: store %0.4f, get %0.4f, remove %0.4f", storeTime, getTime, removeTime))
+        }
+    }
+    
+    private func testDiskCacheKi(with testItems: [TestItem]) -> NoParamterBlock {
+        return {
+            var storeTime: Double = 0
+            var getTime: Double = 0
+            var removeTime: Double = 0
+            func testStore(_ completion: @escaping NoParamterBlock) {
+                var i = testItems.count
+                for item in testItems {
+                    let startTime = CACurrentMediaTime()
+                    KingfisherManager.shared.cache.store(item.image, original: item.data, forKey: item.key, toDisk: true) {
+                        i -= 1
+                        if i == 0 {
+                            storeTime += CACurrentMediaTime() - startTime
+                            KingfisherManager.shared.cache.clearMemoryCache()
+                            completion()
+                        }
+                    }
+                }
+            }
+            func testGet(_ completion: @escaping NoParamterBlock) {
+                var i = testItems.count
+                for item in testItems {
+                    let startTime = CACurrentMediaTime()
+                    KingfisherManager.shared.cache.retrieveImage(forKey: item.key, options: nil) { (image, _) in
+                        assert(image != nil)
+                        i -= 1
+                        if i == 0 {
+                            getTime += CACurrentMediaTime() - startTime
+                            completion()
+                        }
+                    }
+                }
+            }
+            func testRemove(_ completion: @escaping NoParamterBlock) {
+                var i = testItems.count
+                for item in testItems {
+                    let startTime = CACurrentMediaTime()
+                    KingfisherManager.shared.cache.removeImage(forKey: item.key, fromMemory: false, fromDisk: true) {
+                        i -= 1
+                        if i == 0 {
+                            removeTime += CACurrentMediaTime() - startTime
+                            completion()
+                        }
+                    }
+                }
+            }
+            testStore {
+                testGet {
+                    testRemove {
+                        print(String(format: "Kingfisher time consume: store %0.4f, get %0.4f, remove %0.4f", storeTime, getTime, removeTime))
+                        KingfisherManager.shared.cache.clearMemoryCache()
+                    }
+                }
+            }
         }
     }
 }
